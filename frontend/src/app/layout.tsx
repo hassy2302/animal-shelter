be12ad3f3a2730bf7e6ba045e6afc5589990ea8c
milk <milk@munpia.com>
2026-03-27 19:07:35 +0900

    Fix: GTM ID 수정 (GTM-KSHWZ9C6)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,14 +25,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KSHWZ9C');`,
+})(window,document,'script','dataLayer','GTM-KSHWZ9C6');`,
           }}
         />
       </head>
       <body className="min-h-screen bg-[var(--bg)]">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KSHWZ9C"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KSHWZ9C6"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
