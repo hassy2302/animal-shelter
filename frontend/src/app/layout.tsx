595030e20a50f1eaf567a326dbcea7b9a893b84a
milk <milk@munpia.com>
2026-03-27 19:16:39 +0900

    Feat: GA4 직접 연결 (G-3Y4321MKJF)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,28 +18,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <head>
         <Script
-          id="gtm-head"
+          src="https://www.googletagmanager.com/gtag/js?id=G-3Y4321MKJF"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KSHWZ9C6');`,
-          }}
         />
+        <Script id="ga4" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-3Y4321MKJF');`}
+        </Script>
       </head>
-      <body className="min-h-screen bg-[var(--bg)]">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KSHWZ9C6"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {children}
-      </body>
+      <body className="min-h-screen bg-[var(--bg)]">{children}</body>
     </html>
   );
 }
