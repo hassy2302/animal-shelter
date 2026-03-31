dac1964725004529f737841f20eab11545b4746f
milk <milk@munpia.com>
2026-03-31 17:07:47 +0900

    Feat: Sentry wizard 설정 완료

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -1,7 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://d8e7a27db580d9d0c05042be98b6580f@o4511137869463552.ingest.us.sentry.io/4511137875296256",
-  environment: process.env.NODE_ENV,
-  tracesSampleRate: 0.2,
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
